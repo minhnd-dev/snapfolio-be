3a0b41c0d3bf3a84f7f38146507479d8ecbc50f9
minh <nguyenducminh.dev@gmail.com>
2023-09-02 14:03:56 +0700

    Update database

--- alembic/env.py
+++ alembic/env.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from dotenv import load_dotenv
 
 parent_dir = os.path.abspath(os.path.join(os.getcwd(), ".."))
 sys.path.append(parent_dir)
@@ -15,6 +16,7 @@ from core.models.user import User
 from core.models.file import File
 from core.models.tag import Tag
 
+load_dotenv()
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -36,7 +38,7 @@ target_metadata = Base.metadata
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-
+config.set_main_option('sqlalchemy.url', os.environ.get("DATABASE_URL"))
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
--- core/models/database.py
+++ core/models/database.py
@@ -1,7 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from dotenv import load_dotenv
+import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+load_dotenv()
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
